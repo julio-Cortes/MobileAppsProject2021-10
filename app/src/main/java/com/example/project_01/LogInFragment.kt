2820bf5aed20848204b94a45cfe1ae5d782aedb9
julio-Cortes <jicortes1@miuandes.cl>
2021-04-24 20:19:22 -0400

    bases_of_login_fragment

---
+++ app/src/main/java/com/example/project_01/LogInFragment.kt
@@ -0,0 +1,12 @@
+package com.example.project_01
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class LogInFragment : Fragment() {
+
+
+}
